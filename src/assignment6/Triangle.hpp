d65a4504051218e4dc7e729cc0bbae959ff55253
gfgafn <87858323+gfgafn@users.noreply.github.com>
2023-01-30 21:37:56 +0800

    fix(assignment6): Fixed `Triangle::getIntersection(Ray ray)`
    Fixed the intersection of light and triangle

--- src/assignment6/Triangle.hpp
+++ src/assignment6/Triangle.hpp
@@ -213,23 +213,28 @@ inline Intersection Triangle::getIntersection(Ray ray)
     if (dotProduct(ray.direction, normal) > 0)
         return inter;
     double u, v, t_tmp = 0;
-    Vector3f pvec = crossProduct(ray.direction, e2);
-    double det = dotProduct(e1, pvec);
+    Vector3f pvec = crossProduct(ray.direction, e2); // S1
+    double det = dotProduct(e1, pvec);               // S1 · E1
     if (fabs(det) < EPSILON)
         return inter;
 
     double det_inv = 1. / det;
-    Vector3f tvec = ray.origin - v0;
-    u = dotProduct(tvec, pvec) * det_inv;
+    Vector3f tvec = ray.origin - v0;      // S
+    u = dotProduct(tvec, pvec) * det_inv; // b1
     if (u < 0 || u > 1)
         return inter;
-    Vector3f qvec = crossProduct(tvec, e1);
-    v = dotProduct(ray.direction, qvec) * det_inv;
+    Vector3f qvec = crossProduct(tvec, e1);        // S2
+    v = dotProduct(ray.direction, qvec) * det_inv; // b2
     if (v < 0 || u + v > 1)
         return inter;
-    t_tmp = dotProduct(e2, qvec) * det_inv;
+    t_tmp = dotProduct(e2, qvec) * det_inv; // t
 
     // TODO find ray triangle intersection
+
+    if (t_tmp < 0)
+    {
+        return inter;
+    }
 
     inter.happened = true;
     inter.coords = ray(t_tmp);
